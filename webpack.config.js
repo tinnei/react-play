8b4a56c57906280441545e30c1ebd8aec7ecc02a
Tinnei <tinnei.pang@ericsson.com>
2018-12-10 17:53:15 -0800

    Set up production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,49 @@
-const path = require('path');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-    entry: './src/app.js',
+module.exports = env => {
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
+
+  return {
+    entry: "./src/app.js",
     output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js"
     },
     module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
             use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true
+                }
+              }
             ]
-        }]
+          })
+        }
+      ]
     },
-    devtool: 'cheap-module-eval-source-map',
+    plugins: [CSSExtract],
+    devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true
     }
+  };
 };
-
